83208633894260df0ec02cd8a9e5083d2f750ec7
Wato Elle <watoelle@gmail.com>
2024-06-26 22:22:46 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ${Grace Hopper}!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting = 'Welcome, $G{currentUser.slice(0,1)}!';
